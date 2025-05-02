4ab700a31749481062225c30306c981a155649d9
james.eastham <james.eastham@datadoghq.com>
2025-05-02 20:25:45 +1000

    Add install of Azure function tools

---
+++ user-management/src/Stickerlandia.UserManagement.Agnostic/KafakStickerClaimedWorker.cs
@@ -0,0 +1,82 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025 Datadog, Inc.
+
+using System.Text.Json;
+using Azure.Messaging.ServiceBus;
+using Datadog.Trace;
+using Microsoft.Extensions.Logging;
+using Saunter.Attributes;
+using Stickerlandia.UserManagement.Core;
+using Stickerlandia.UserManagement.Core.StickerClaimedEvent;
+
+namespace Stickerlandia.UserManagement.Azure;
+
+[AsyncApi]
+public class ServiceBusStickerClaimedWorker : IMessagingWorker
+{
+    private readonly StickerClaimedEventHandler _eventHandler;
+    private readonly ILogger<ServiceBusStickerClaimedWorker> _logger;
+    private readonly ServiceBusProcessor _processor;
+
+    public ServiceBusStickerClaimedWorker(StickerClaimedEventHandler eventHandler, ILogger<ServiceBusStickerClaimedWorker> logger, ServiceBusClient serviceBusClient)
+    {
+        _eventHandler = eventHandler;
+        _logger = logger;
+        
+        // Create the processor
+        _processor = serviceBusClient.CreateProcessor("users.stickerClaimed.v1");
+        
+        // Set up handlers
+        _processor.ProcessMessageAsync += ProcessMessageAsync;
+        _processor.ProcessErrorAsync += ProcessErrorAsync;
+    }
+    
+    [Channel("users.stickerClaimed.v1")]
+    [SubscribeOperation(typeof(StickerClaimedEventV1))]
+    private async Task ProcessMessageAsync(ProcessMessageEventArgs args)
+    {
+        using var processSpan = Tracer.Instance.StartActive($"process users.stickerClaimed.v1");
+        
+        var messageBody = args.Message.Body.ToString();
+        _logger.LogInformation("Received message: {body}", messageBody);
+
+        var evtData = JsonSerializer.Deserialize<StickerClaimedEventV1>(messageBody);
+
+        if (evtData == null)
+        {
+            await args.DeadLetterMessageAsync(args.Message, "Message body cannot be deserialized");
+        }
+        
+        // Process your message here
+        await _eventHandler.Handle(evtData!);
+        
+        // Complete the message
+        await args.CompleteMessageAsync(args.Message, CancellationToken.None);
+    }
+    
+    private Task ProcessErrorAsync(ProcessErrorEventArgs args)
+    {
+        _logger.LogError(args.Exception, "Error processing message: {source}", args.ErrorSource);
+        return Task.CompletedTask;
+    }
+
+    public Task StartAsync()
+    {
+        _logger.LogInformation("Starting ServiceBus processor");
+        _processor.StartProcessingAsync();
+        return Task.CompletedTask;
+    }
+
+    public Task PollAsync()
+    {
+        // This should be a no-op;
+        return Task.CompletedTask;
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await _processor.StopProcessingAsync();
+        await _processor.CloseAsync(cancellationToken);
+    }
+}
---
+++ user-management/src/Stickerlandia.UserManagement.Agnostic/ServiceExtensions.cs
@@ -0,0 +1,83 @@
+// Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+// This product includes software developed at Datadog (https://www.datadoghq.com/).
+// Copyright 2025 Datadog, Inc.
+
+using Azure.Messaging.ServiceBus;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Stickerlandia.UserManagement.Core;
+using Stickerlandia.UserManagement.Core.Outbox;
+
+namespace Stickerlandia.UserManagement.Azure;
+
+public static class ServiceExtensions
+{
+    public static FunctionsApplicationBuilder AddAzureAdapters(this FunctionsApplicationBuilder builder)
+    {
+        builder.AddAzureCosmosClient(
+            "cosmosdb",
+            settings => { settings.DisableTracing = false; },
+            clientOptions =>
+            {
+                clientOptions.ApplicationName = "cosmos-aspire";
+                clientOptions.SerializerOptions = new CosmosSerializationOptions
+                {
+                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.Default
+                };
+                clientOptions.CosmosClientTelemetryOptions = new CosmosClientTelemetryOptions
+                {
+                    DisableDistributedTracing = false
+                };
+                clientOptions.ConnectionMode = ConnectionMode.Gateway;
+            });
+
+        builder.Services.AddServices(builder.Configuration);
+
+        return builder;
+    }
+
+    public static WebApplicationBuilder AddAzureAdapters(this WebApplicationBuilder builder)
+    {
+        builder.AddAzureCosmosClient(
+            "cosmosdb",
+            settings =>
+            {
+                settings.DisableTracing = false;
+                settings.ConnectionString = builder.Configuration
+                    .GetConnectionString("cosmosdb");
+            },
+            clientOptions =>
+            {
+                clientOptions.SerializerOptions = new CosmosSerializationOptions
+                {
+                    PropertyNamingPolicy = CosmosPropertyNamingPolicy.Default
+                };
+                clientOptions.CosmosClientTelemetryOptions = new CosmosClientTelemetryOptions
+                {
+                    DisableDistributedTracing = false
+                };
+                clientOptions.ConnectionMode = ConnectionMode.Gateway;
+            });
+
+        builder.Services.AddServices(builder.Configuration);
+
+        return builder;
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddSingleton<IMessagingWorker, ServiceBusStickerClaimedWorker>();
+        services.AddSingleton(new ServiceBusClient(configuration["ConnectionStrings:messaging"]));
+
+        // RegisterUser the CosmosDB repository implementation
+        services.AddSingleton<IUsers, CosmosDbUserRepository>();
+        services.AddSingleton<IOutbox, CosmosDbUserRepository>();
+        services.AddSingleton<IUserEventPublisher, ServiceBusEventPublisher>();
+
+        return services;
+    }
+}
