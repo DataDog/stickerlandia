d5d5b598f5a776a4e06f401c6abd354d9fd93ed5
Scott Gerring <scott@scottgerring.com>
2025-05-07 13:28:12 +0200

    chore: tests for endpoints

---
+++ sticker-award/src/test/java/com/datadoghq/stickerlandia/stickeraward/HealthResourceIT.java
@@ -0,0 +1,8 @@
+package com.datadoghq.stickerlandia.stickeraward;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HealthResourceIT extends HealthResourceTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ sticker-award/src/test/java/com/datadoghq/stickerlandia/stickeraward/HealthResourceTest.java
@@ -0,0 +1,24 @@
+package com.datadoghq.stickerlandia.stickeraward;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+
+@QuarkusTest
+public class HealthResourceTest {
+
+    @Test
+    void testHealthEndpoint() {
+        given()
+            .when().get("/health")
+            .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("ok", is(true))
+                .body("timestamp", notNullValue());
+    }
+}
