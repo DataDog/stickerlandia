75835368bce6e915d9f7e124940e5d0a0fe1f44f
james.eastham <james.eastham@datadoghq.com>
2025-07-23 11:22:26 +0100

    feat: add shared infra for AWS deployment

---
+++ shared-infra/aws/bin/shared-resources.ts
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+import * as cdk from "aws-cdk-lib";
+import { StickerlandiaSharedResourcesStack } from "../lib/shared-resources-stack";
+import { AwsSolutionsChecks, ServerlessChecks } from "cdk-nag";
+
+const env = process.env.ENV || "dev";
+
+const app = new cdk.App();
+new StickerlandiaSharedResourcesStack(
+  app,
+  `StickerlandiaSharedResources-${env}`,
+  {}
+);
+cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
+cdk.Aspects.of(app).add(new ServerlessChecks({ verbose: true }));
+
+cdk.Tags.of(app).add("project", "stickerlandia");
+cdk.Tags.of(app).add("team", "platform");
+cdk.Tags.of(app).add("env", env);
