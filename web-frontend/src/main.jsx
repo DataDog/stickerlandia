465ac2c20bbdee7c405f73fd182ca23d90952729
Scott Gerring <scott@scottgerring.com>
2025-08-14 14:10:35 +0200

    feat(web-frontend): setup RUM

--- web-frontend/src/main.jsx
+++ web-frontend/src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { initializeDatadogRum } from './services/DatadogRum.js'
+
+// Initialize Datadog RUM
+initializeDatadogRum();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- web-frontend/vite.config.js
+++ web-frontend/vite.config.js
@@ -2,10 +2,11 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteEnvs } from "vite-envs";
 
 export default defineConfig({
  base: "/",
- plugins: [react()],
+ plugins: [react(), viteEnvs()],
  preview: {
   port: 5173,
   strictPort: true,
