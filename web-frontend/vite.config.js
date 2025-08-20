3b554670b409efe018c6a0cd4e77a6894ed3bf39
Rachel White <loveless@gmail.com>
2025-08-20 16:17:41 -0400

    readding tailwind in as vite plugin

--- web-frontend/vite.config.js
+++ web-frontend/vite.config.js
@@ -2,11 +2,12 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"
 import { viteEnvs } from "vite-envs";
 
 export default defineConfig({
  base: "/",
- plugins: [react(), viteEnvs()],
+ plugins: [react(), viteEnvs(), tailwindcss()],
  preview: {
   port: 5173,
   strictPort: true,
