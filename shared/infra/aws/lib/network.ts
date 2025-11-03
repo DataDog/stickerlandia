1add23b61882afed3f46a6f1b1377ba908b5b530
james.eastham <james.eastham@datadoghq.com>
2025-11-03 08:04:18 +0000

    feat: add AWS shared infra

---
+++ shared/infra/aws/lib/network.ts
@@ -0,0 +1,174 @@
+/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2025-Present Datadog, Inc.
+ */
+
+import {
+  CfnRoute,
+  FlowLogDestination,
+  GatewayVpcEndpointAwsService,
+  IVpc,
+  Peer,
+  Port,
+  SecurityGroup,
+  SubnetType,
+  Vpc,
+} from "aws-cdk-lib/aws-ec2";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
+import { VpcLink } from "aws-cdk-lib/aws-apigatewayv2";
+import { Construct } from "constructs";
+import {
+  AllowedMethods,
+  CachePolicy,
+  Distribution,
+  OriginProtocolPolicy,
+  OriginRequestPolicy,
+  ResponseHeadersPolicy,
+  SecurityPolicyProtocol,
+} from "aws-cdk-lib/aws-cloudfront";
+import {
+  ApplicationListener,
+  ApplicationLoadBalancer,
+  ApplicationProtocol,
+  ListenerAction,
+  SslPolicy,
+} from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { LoadBalancerV2Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+
+export interface NetworkProps {
+  env: string;
+}
+
+export class Network extends Construct {
+  vpc: IVpc;
+  vpcLink: VpcLink;
+  noInboundAllOutboundSecurityGroup: SecurityGroup;
+  loadBalancer: ApplicationLoadBalancer;
+  applicationListener: ApplicationListener;
+
+  constructor(scope: Construct, id: string, props: NetworkProps) {
+    super(scope, id);
+    this.vpc = new Vpc(this, "Vpc", {
+      vpcName: `stickerlandia-${props.env}-vpc`,
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: "Public",
+          subnetType: SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: "Private",
+          subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        {
+          cidrMask: 24,
+          name: "Isolated",
+          subnetType: SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
+
+    this.vpc.addGatewayEndpoint("dynamoDBEndpoint", {
+      service: GatewayVpcEndpointAwsService.DYNAMODB,
+    });
+    this.vpc.addGatewayEndpoint("s3Endpoint", {
+      service: GatewayVpcEndpointAwsService.S3,
+    });
+
+    this.noInboundAllOutboundSecurityGroup = new SecurityGroup(
+      this,
+      "noInboundAllOutboundSecurityGroup",
+      {
+        vpc: this.vpc,
+        allowAllOutbound: true,
+        description: "No inbound / all outbound",
+        securityGroupName: "noInboundAllOutboundSecurityGroup",
+      }
+    );
+    this.noInboundAllOutboundSecurityGroup.addIngressRule(
+      this.noInboundAllOutboundSecurityGroup,
+      Port.tcp(8080),
+      "allow self"
+    );
+    this.noInboundAllOutboundSecurityGroup.addIngressRule(
+      Peer.ipv4(this.vpc.vpcCidrBlock),
+      Port.tcp(8080)
+    );
+
+    this.vpcLink = new VpcLink(this, "HttpApiVpcLink", {
+      vpcLinkName: `Stickerlandia-${props.env}-VpcLink`,
+      vpc: this.vpc,
+      subnets: this.vpc.selectSubnets({
+        subnetType: SubnetType.PRIVATE_WITH_EGRESS,
+      }),
+      securityGroups: [this.noInboundAllOutboundSecurityGroup],
+    });
+
+    const vpcIdParameter = new StringParameter(this, "VPCIdParameter", {
+      stringValue: this.vpc.vpcId,
+      parameterName: `/stickerlandia/${props.env}/shared/vpc-id`,
+    });
+
+    const vpcLinkSgParameter = new StringParameter(
+      this,
+      "VPCLinkSecurityGroupParameter",
+      {
+        stringValue: this.noInboundAllOutboundSecurityGroup.securityGroupId,
+        parameterName: `/stickerlandia/${props.env}/shared/vpc-link-sg-id`,
+      }
+    );
+
+    const loadBalancerSecurityGroup = new SecurityGroup(
+      this,
+      "LoadBalancerSG",
+      {
+        vpc: this.vpc,
+      }
+    );
+    loadBalancerSecurityGroup.addEgressRule(
+      Peer.anyIpv4(),
+      Port.allTraffic(),
+      "Allow all outbound traffic"
+    );
+    // Cloudfront Prefix List.
+    loadBalancerSecurityGroup.addIngressRule(
+      Peer.prefixList("pl-4fa04526"),
+      Port.tcp(80),
+      "Allow HTTPS traffic from anywhere"
+    );
+
+    this.loadBalancer = new ApplicationLoadBalancer(this, "StickerlandiaALB", {
+      vpc: this.vpc,
+      internetFacing: true,
+      loadBalancerName: `StickerlandiaALB-${props.env}`,
+      securityGroup: loadBalancerSecurityGroup,
+    });
+
+    this.applicationListener = this.loadBalancer.addListener("HTTPSListener", {
+      port: 80,
+      open: true,
+      protocol: ApplicationProtocol.HTTP,
+      certificates: [],
+      defaultAction: ListenerAction.fixedResponse(404),
+    });
+
+    const distribution = new Distribution(this, `Stickerlandia-${props.env}`, {
+      minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
+      defaultBehavior: {
+        origin: new LoadBalancerV2Origin(this.loadBalancer, {
+          protocolPolicy: OriginProtocolPolicy.HTTP_ONLY,
+        }),
+        compress: false,
+        cachePolicy: CachePolicy.CACHING_DISABLED,
+        allowedMethods: AllowedMethods.ALLOW_ALL,
+        originRequestPolicy: OriginRequestPolicy.ALL_VIEWER,
+        responseHeadersPolicy:
+          ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS_WITH_PREFLIGHT_AND_SECURITY_HEADERS,
+      },
+    });
+  }
+}
