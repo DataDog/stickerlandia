27f577e9a990ef23842898ba77570bb22d8d7d64
james.eastham <james.eastham@datadoghq.com>
2026-01-14 07:16:43 +0000

    chore: add copyright header

--- user-management/src/Stickerlandia.UserManagement.Agnostic/Migrations/20260112134430_Net10Upgrade.cs
+++ user-management/src/Stickerlandia.UserManagement.Agnostic/Migrations/20260112134430_Net10Upgrade.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿/*
+ * Unless explicitly stated otherwise all files in this repository are licensed under the Apache License Version 2.0.
+ * This product includes software developed at Datadog (https://www.datadoghq.com/).
+ * Copyright 2025-Present Datadog, Inc.
+ */
+
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #pragma warning disable
 #nullable disable
